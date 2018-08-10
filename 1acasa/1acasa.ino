7772a2fe193e7f4806d86005b9d8cafc6a908a69
hagauiulian <42179375+hagauiulian@users.noreply.github.com>
2018-08-10 04:34:52 +0300

    pot doar de pe web

--- 1acasa/1acasa.ino
+++ 1acasa/1acasa.ino
@@ -1,24 +1,74 @@
-#define PIN_YELLOW 12
-#define PIN_RED 11
-#define PIN_GREEN 10
+#define PIN_12 12
+#define PIN_11 11
+#define PIN_10 10
+#define PIN_9 9
+#define PIN_8 8
+#define PIN_7 7
+#define PIN_6 6
+#define PIN_3 3
+#define PIN_2 2
+#define SPEAKER 4
 void setup() {
-  pinMode(PIN_YELLOW, OUTPUT);
-  pinMode(PIN_RED, OUTPUT);
-  pinMode(PIN_GREEN, OUTPUT);
-
+  pinMode(PIN_12, OUTPUT);
+  pinMode(PIN_11, OUTPUT);
+  pinMode(PIN_10, OUTPUT);
+  pinMode(PIN_9, OUTPUT);
+  pinMode(PIN_8, OUTPUT);
+  pinMode(PIN_7, OUTPUT);
+  pinMode(PIN_6, OUTPUT);
+  pinMode(PIN_3, OUTPUT);
+  pinMode(PIN_2, OUTPUT);
+  pinMode(SPEAKER, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(PIN_YELLOW, HIGH);
-  digitalWrite(PIN_RED, HIGH);
-  digitalWrite(PIN_GREEN, HIGH);
+  digitalWrite(PIN_12, HIGH);
+  tone(SPEAKER, 1000, 90);
+  delay(100);
+  digitalWrite(PIN_11, HIGH);
+  tone(SPEAKER, 800, 90);
+  delay(100);
+  digitalWrite(PIN_10, HIGH);
+  tone(SPEAKER, 900, 90);
+  delay(100);
+  digitalWrite(PIN_9, HIGH);
+  tone(SPEAKER, 1000, 90);
+  delay(100);
+  digitalWrite(PIN_8, HIGH);
+  tone(SPEAKER, 900, 90);
+  delay(100);
+  digitalWrite(PIN_7, HIGH);
+  tone(SPEAKER, 800, 90);
+  delay(100);
+  digitalWrite(PIN_6, HIGH);
+  tone(SPEAKER, 900, 90);
+  delay(100);
+  digitalWrite(PIN_3, HIGH);
+  tone(SPEAKER, 1000, 90);
+  delay(100);
+  digitalWrite(PIN_2, HIGH);
+  tone(SPEAKER, 1500, 90);
   
   delay(1000);
   
-  digitalWrite(PIN_YELLOW, LOW);
-  digitalWrite(PIN_RED, LOW);
-  digitalWrite(PIN_GREEN, LOW);
-
+  digitalWrite(PIN_12, LOW);
+  delay(100);
+  digitalWrite(PIN_11, LOW);
+  delay(100);
+  digitalWrite(PIN_10, LOW);
+  delay(100);
+  digitalWrite(PIN_9, LOW);
+  delay(100);
+  digitalWrite(PIN_8, LOW);
+  delay(100);
+  digitalWrite(PIN_7, LOW);
+  delay(100);
+  digitalWrite(PIN_6, LOW);
+  delay(100);
+  digitalWrite(PIN_3, LOW);
+  delay(100);
+  digitalWrite(PIN_2, LOW);
+  
   delay(1000);
 
 }
