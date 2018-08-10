12c8da610321cd7d2282cd91ccce17d631687626
hagauiulian <42179375+hagauiulian@users.noreply.github.com>
2018-08-10 03:44:47 +0300

    Add files via upload
    o incercare

---
+++ 1acasa.ino
@@ -0,0 +1,24 @@
+#define PIN_YELLOW 12
+#define PIN_RED 11
+#define PIN_GREEN 10
+void setup() {
+  pinMode(PIN_YELLOW, OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(PIN_YELLOW, HIGH);
+  digitalWrite(PIN_RED, HIGH);
+  digitalWrite(PIN_GREEN, HIGH);
+  
+  delay(1000);
+  
+  digitalWrite(PIN_YELLOW, LOW);
+  digitalWrite(PIN_RED, LOW);
+  digitalWrite(PIN_GREEN, LOW);
+
+  delay(1000);
+
+}
